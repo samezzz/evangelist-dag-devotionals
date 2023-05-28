2318f4422fb3cec2b2df9b948b49c531726a95f9
samezzz <95895530+samezzz@users.noreply.github.com>
2023-05-28 03:44:30 +0000

    altered footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import { FaTwitter, FaYoutube, FaFacebook } from "react-icons/fa"
-import Link from "next/link"
+import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <section className="flex flex-col">
-      <div className="flex flex-row justify-center align-middle gap-4 text-4xl lg:text-5xl">
-        <Link href="https://www.youtube.com/@DagHewardMillsvideos">
-          <FaYoutube className="w-9 h-9 text-[#c4302b]" />
-        </Link>
-        <Link href="https://facebook.com/daghewardmills.org">
-          <FaFacebook className="w-8 h-8 text-[#3b5998]" />
-        </Link>
-        <Link href="https://twitter.com/EvangelistDag">
-          <FaTwitter className="w-8 h-8 text-[#1DA1F2]" />
-        </Link>
+    <footer className="border-t-[0.1px] border-t-gray-400 dark:border-t-[#302e2e] text-gray-500 dark:text-gray-200">
+      <div className="container flex flex-col-reverse justify-between px-6 pb-5 mx-auto space-y-5 md:flex-row md:space-y-0">
+        {/* Logo and Social links container */}
+        <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
+          <div className="mx-auto my-6 text-center md:hidden">&copy; 2023, DAILY COUNSEL. All rights reserved.</div>
+        </div>
+        {/* List Container */}
+        <div className="flex justify-around space-x-32">
+          <div className="flex flex-col space-y-3">
+            <Link href="/" className="hover:text-orange-500 text-blue-500 underline">Home</Link>
+          </div>
+          <div className="flex flex-col space-y-3">
+            <Link href="/" className="hover:text-orange-500 text-blue-500 underline">About</Link>
+          </div>
+        </div>
+        {/* Input Container */}
+        <div className="flex flex-col justify-between">
+        <div className="hidden md:block ">&copy; 2023, DAILY COUNSEL. All rights reserved.</div>
+        </div>
       </div>
-      <div className="mx-auto my-6 text-center text-gray-400 ">&copy; 2023 DAILY COUNSEL. All rights reserved.
-      </div>
-    </section>
-  )
+    </footer>
+  );
 }
