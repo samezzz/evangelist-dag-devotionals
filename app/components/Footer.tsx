05e5fe36f3d5b23e812215ac1f64af3659d14ac0
samezzz <95895530+samezzz@users.noreply.github.com>
2023-05-28 03:53:44 +0000

    worked on footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="border-t-[0.1px] border-t-gray-400 dark:border-t-[#302e2e] text-gray-500 dark:text-gray-200">
-      <div className="container flex flex-col-reverse justify-between px-6 pb-5 mx-auto space-y-5 md:flex-row md:space-y-0">
+      <div className="container flex flex-col-reverse justify-between px-6 pb-5 mx-auto space-y-6 md:flex-row md:space-y-3">
         {/* Logo and Social links container */}
         <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
           <div className="mx-auto my-6 text-center md:hidden">&copy; 2023, DAILY COUNSEL. All rights reserved.</div>
@@ -11,15 +11,19 @@ export default function Footer() {
         {/* List Container */}
         <div className="flex justify-around space-x-32">
           <div className="flex flex-col space-y-3">
+            <p>SECTION</p>  
             <Link href="/" className="hover:text-orange-500 text-blue-500 underline">Home</Link>
+            <Link href="/" className="hover:text-orange-500 text-blue-500 underline">About</Link>
           </div>
           <div className="flex flex-col space-y-3">
-            <Link href="/" className="hover:text-orange-500 text-blue-500 underline">About</Link>
+            <p>RELATED</p>
+            <Link href="https://www.youtube.com/@FaithDigitalNetwork" className="hover:text-orange-500 text-blue-500 underline">FaithDigital Network</Link>
+            <Link href="https://www.youtube.com/@firstlovecenter" className="hover:text-orange-500 text-blue-500 underline">First Love Center</Link>
           </div>
         </div>
         {/* Input Container */}
         <div className="flex flex-col justify-between">
-        <div className="hidden md:block ">&copy; 2023, DAILY COUNSEL. All rights reserved.</div>
+        <div className="hidden md:block">&copy; 2023, DAILY COUNSEL. All rights reserved.</div>
         </div>
       </div>
     </footer>
