bfc9d4a7c18dfb8856458e74b78c84e015b85c5d
samezzz <95895530+samezzz@users.noreply.github.com>
2023-05-28 01:19:37 +0000

    removed some eslint rules

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,7 +9,7 @@ const manuale = Manuale({
 export default function Hero() {
   return (
     <section className={`${manuale.variable} font-manuale w-full max-w-screen mx-auto`}>
-      <h1 className="mt-12 mb-12 text-5xl md:text-7xl text-center dark:text-gray-100 font-bold leading-snug tracking-widest ">MINDSHARE <br /><span className="text-gradient-02">MEMOIRS</span>
+      <h1 className="mt-12 mb-12 text-5xl md:text-7xl text-center dark:text-gray-100 font-bold leading-snug tracking-widest ">DAILY <br /><span className="text-gradient-02">COUNSEL</span>
       </h1>
     </section>
   )
