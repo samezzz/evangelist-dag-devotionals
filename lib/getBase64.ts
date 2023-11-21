04ff5b700b2bfe32474a630683031713bb2d42a9
Samuel Essilfie <PsalmuelSelfie@gmail.com>
2023-11-21 03:18:38 +0000

    base64 on hold

--- lib/getBase64.ts
+++ lib/getBase64.ts
@@ -1,32 +1,32 @@
-import { Meta } from "@/types";
-import { getPlaiceholder } from "plaiceholder";
+// import { Meta } from "@/types";
+// import { getPlaiceholder } from "plaiceholder";
 
-async function getBase64(imageUrl: string) {
-    try {
-        const res = await fetch(imageUrl)
+// async function getBase64(imageUrl: string) {
+//     try {
+//         const res = await fetch(imageUrl)
 
-        if (!res.ok) {
-            throw new Error(`Failed to fetch image: ${res.status} ${res.statusText}`)
-        }
+//         if (!res.ok) {
+//             throw new Error(`Failed to fetch image: ${res.status} ${res.statusText}`)
+//         }
 
-        const buffer = await res.arrayBuffer()
+//         const buffer = await res.arrayBuffer()
 
-        const { base64 } = await getPlaiceholder(Buffer.from(buffer))
+//         const { base64 } = await getPlaiceholder(Buffer.from(buffer))
 
-        return base64
-    }  catch(error) {
-        if(error instanceof Error) console.log(error.stack) 
-    }
-}
+//         return base64
+//     }  catch(error) {
+//         if(error instanceof Error) console.log(error.stack) 
+//     }
+// }
 
-export default async function addBlurredDataUrls(images: Meta[]): Promise<Meta> {
-    const base64Promises = images.map(item => getBase64(item.imgSrc || ''))
-    const base64Results = await Promise.all(base64Promises)
+// export default async function addBlurredDataUrls(images: Meta[]): Promise<Meta> {
+//     const base64Promises = images.map(item => getBase64(item.imgSrc || ''))
+//     const base64Results = await Promise.all(base64Promises)
 
-    const photosWithBlur: Meta = images.map((item, i) => ({
-        ...item,
-        blurredDataUrls: base64Results[i],
-    }));
+//     const photosWithBlur: Meta = images.map((item, i) => ({
+//         ...item,
+//         blurredDataUrls: base64Results[i],
+//     }));
 
-    return photosWithBlur;
-}
+//     return photosWithBlur;
+// }
