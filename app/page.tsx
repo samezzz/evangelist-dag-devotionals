3378aeb7c26f928f354b29cd7d9b7cd8f2c28460
Samuel Essilfie <PsalmuelSelfie@gmail.com>
2023-11-22 01:53:44 +0000

    it should work now

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="overflow-x-hidden max-w-screen">
       <Hero />
-      {/* <Info /> */}
+      {/* <Info /> */} 
     </main>
   );
 }
