b62f63061336bd10239570458a682e7a87f7e25d
Samuel Essilfie <PsalmuelSelfie@gmail.com>
2023-11-22 04:07:36 +0000

    fixed intersection observer

--- components/InfiniteScrollPosts.tsx
+++ components/InfiniteScrollPosts.tsx
@@ -1,50 +1,77 @@
-'use client'
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import PostItem from './PostItem'
-import { Meta } from '@/types'
-import { useInView } from 'react-intersection-observer'
-import { fetchPosts } from '@/app/posts/actions'
-import { Loader2 } from 'lucide-react'
+import React, { useEffect, useState, useRef } from "react";
+import PostItem from "./PostItem";
+import { Meta } from "@/types";
+import { fetchPosts } from "@/app/posts/actions";
+import { Loader2 } from "lucide-react";
 
 type InfiniteScrollPostsProps = {
   initialPosts: Meta[];
 };
 
-const InfiniteScrollPosts: React.FC<InfiniteScrollPostsProps> = ({ initialPosts }) => {
+const InfiniteScrollPosts: React.FC<InfiniteScrollPostsProps> = ({
+  initialPosts,
+}) => {
+  const [posts, setPosts] = useState(initialPosts);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const loaderRef = useRef<HTMLDivElement | null>(null);
 
-    const [posts, setPosts] = useState(initialPosts)
-    const [page, setPage ] = useState(1)
-    const [ref, inView] = useInView()
+  const loadMorePosts = async () => {
+    if (!isLoading && hasMore) {
+      setIsLoading(true);
+      const next = page + 1;
+      const newPosts = await fetchPosts({ page: next });
+      if (newPosts?.length) {
+        setPage(next);
+        setPosts((prev) => [...prev, ...newPosts]);
+      } else {
+        setHasMore(false);
+      }
+      setIsLoading(false);
+    }
+  };
 
-    async function loadMorePosts() {
-        const next = page + 1
-        const posts = await fetchPosts({page: next});
-        if(posts?.length) {
-            setPage(next)
-            setPosts((prev: Meta[] | undefined) => [
-                ...(prev?.length ? prev : []),
-                ...posts
-            ])
-        }
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "20px", // Adjust as needed to trigger the loading slightly before reaching the bottom
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting && !isLoading && hasMore) {
+        loadMorePosts();
+      }
+    }, options);
+
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
     }
 
-    useEffect(() => {
-        if(inView) {
-            loadMorePosts()
-        }
-    }, [inView])
+    return () => {
+      if (loaderRef.current) {
+        observer.unobserve(loaderRef.current);
+      }
+    };
+  }, [isLoading, hasMore]);
 
   return (
     <>
-        {posts.map((post, index) => (
-          <PostItem post={post} key={index} />
+      {posts.map((post, index) => (
+        <PostItem post={post} key={index} />
       ))}
-      <div ref={ref} className='text-center mx-auto font-bold'>
-        <Loader2 className="h-6 w-6 animate-spin" />
-      </div>
+      {isLoading && (
+        <div className="text-center mx-auto font-bold">
+          <Loader2 className="h-6 w-6 animate-spin mb-5 items-center text-center" />
+        </div>
+      )}
+      {/* Trigger for loading more */}
+      {hasMore && <div ref={loaderRef}></div>}
     </>
-  )
-}
+  );
+};
 
-export default InfiniteScrollPosts
+export default InfiniteScrollPosts;
