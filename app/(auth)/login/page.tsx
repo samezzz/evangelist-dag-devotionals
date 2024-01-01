98a992e4aab767a3772c3dad8cd76f0a90da09be
Samuel Essilfie <PsalmuelSelfie@gmail.com>
2024-01-01 09:22:35 +0000

    fix: forgot something

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -15,18 +15,6 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <Link
-        href="/"
-        className={cn(
-          buttonVariants({ variant: "ghost" }),
-          "absolute left-4 top-4 md:left-8 md:top-8"
-        )}
-      >
-        <>
-          <Icons.chevronLeft className="mr-2 h-4 w-4" />
-          Back
-        </>
-      </Link>
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 items-center">
           <Avatar>
