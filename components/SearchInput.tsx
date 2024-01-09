9ffa03cc6d219759f894f52ab4c5e4767cbd4750
samezzz <psalmuelselfie@gmail.com>
2024-01-09 15:44:31 +0000

    fix: revalidate

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -5,36 +5,41 @@ import { Input } from "./ui/input";
 import { useRouter } from "next/navigation";
 import { useDebounce } from "use-debounce";
 import { Icons } from "./Icons";
-import { revalidatePath } from "next/cache";
+import { revalidate } from "@/app/posts/actions";
 
 const SearchInput = ({ search }: { search?: string }) => {
-  const router = useRouter();
-  const [text, setText] = useState(search);
-  const [query] = useDebounce(text, 50);
-  const initialRender = useRef(true);
+	const router = useRouter();
+	const [text, setText] = useState(search);
+	const [query] = useDebounce(text, 50);
+	const initialRender = useRef(true);
 
-  useEffect(() => {
-    if (initialRender.current) {
-      initialRender.current = false;
-      return;
-    }
+	useEffect(() => {
+		if (initialRender.current) {
+			initialRender.current = false;
+			return;
+		}
 
-    if (!query) {
-      router.push(`/posts`);
-    } else {
-      router.push(`/posts?search=${query}`);
-    }
-  }, [query, router]);
-  return (
-    <div className="flex">
-      <Icons.refresh className={`ml-4 mr-2 mt-2 cursor-pointer hover:rotate-90 transition-all duration-500`} onClick={() => {revalidatePath('/posts')}} />
-      <Input
-        className="md:min-w-[330px] lg:min-w-[450px]"
-        placeholder="search"
-        onChange={(e) => setText(e.target.value)}
-      />
-    </div>
-  );
+		if (!query) {
+			router.push(`/posts`);
+		} else {
+			router.push(`/posts?search=${query}`);
+		}
+	}, [query, router]);
+	return (
+		<div className="flex">
+			<Icons.refresh
+				className={`ml-4 mr-2 mt-2 cursor-pointer hover:rotate-90 transition-all duration-500`}
+				onClick={() => {
+					revalidate();
+				}}
+			/>
+			<Input
+				className="md:min-w-[330px] lg:min-w-[450px]"
+				placeholder="search"
+				onChange={(e) => setText(e.target.value)}
+			/>
+		</div>
+	);
 };
 
 export default SearchInput;
