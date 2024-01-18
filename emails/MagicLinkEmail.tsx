445d45e65a07bc566c825c324fa9eb1841d6d346
Samuel Essilfie <PsalmuelSelfie@gmail.com>
2024-01-18 12:57:42 +0000

    fix: removed identity

--- emails/MagicLinkEmail.tsx
+++ emails/MagicLinkEmail.tsx
@@ -26,15 +26,6 @@ export default function MagicLinkEmail({
 			<Tailwind>
 				<Body className="mx-auto my-auto bg-white font-sans">
 					<Container className="mx-auto my-10 max-w-[500px] rounded border border-solid border-gray-200 px-10 py-5">
-						<Section className="mt-8">
-							<Img
-								src="/quietTime.jpeg"
-								width="40"
-								height="40"
-								alt="Daily Counsel"
-								className="mx-auto my-0"
-							/>
-						</Section>
 						<Heading className="mx-0 my-7 p-0 text-center text-xl font-semibold text-black">
 							Welcom to Daily Counsel
 						</Heading>
